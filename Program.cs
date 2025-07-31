a24d4e335fd0b3bcb064cc438333ed75d2071d39
Dieguim <Diegofranciscodesouza7@gmail.com>
2025-07-30 22:18:41 -0300

    ..

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, World!");
+        DateTime dateTime = DateTime.Now.AddYears(5);
+        Console.WriteLine(dateTime.ToString("dd/MM/yyyy HH:mm\n"));
+
+        int num = Convert.ToInt32("5");
+        int num2 = int.Parse("12");
+        Console.WriteLine($"Usando Convert: {num}\nUsando Parse: {num2}");
+
+
+        long num3 = long.MaxValue;
+        Console.WriteLine(num3);
+
+    }
+} 
--- Program2.cs
+++
@@ -1,8 +0,0 @@
-public class Program2
-{
-    public static void Main(String[] args)
-    {
-        DateTime datetime = DateTime.Now;
-        Console.WriteLine(datetime);
-    }
-}
